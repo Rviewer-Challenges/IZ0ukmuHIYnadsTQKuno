01b20e95cccdb613f813c14afeddac174c76735b
Ivaan2 <ivan.m.m.2002.ivi@gmail.com>
2022-09-09 11:42:42 +0200

    NegativeDecisionActivity id definidos

---
+++ projecto-CareerAdviser/app/src/main/java/com/careeradviser/LearningRoute/NegativeDecisionActivity.java
@@ -0,0 +1,14 @@
+package com.careeradviser.LearningRoute;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.careeradviser.R;
+
+public class NegativeDecisionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_negative_decision);
+    }
+}
